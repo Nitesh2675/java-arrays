74ed147a8f95aca1dfeb100b5c68be47d7505f7f
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-03-03 21:38:31 +0530

    [Nitesh] ADD: age checker

---
+++ AgeChecker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class AgeChecker{
+
+public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int arr[]=new int[10];
+	for(int i=0;i<10;i++){
+	arr[i]=sc.nextInt();	
+	}
+	for(int i=0;i<arr.length;i++){
+		if(arr[i]>=18){
+			System.out.println("The student with the age "+arr[i]+" can vote.");
+		}
+		else if(arr[i]<0){
+            System.out.print("Invalid");			
+		}
+		else{
+			System.out.println("The student with the age "+arr[i]+" cannot vote.");
+		}
+		
+	}
+	
+}
+
+}
